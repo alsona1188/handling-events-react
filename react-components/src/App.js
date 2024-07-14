12557dafd8909905a7127c94acc922518433b84a
Alsona Natsi <alsona.naci1188@gmail.com>
2024-07-14 00:30:20 +0000

    Biding the event handler in React

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import FunctionalGreeting from "./components/FunctionalGreeting";
-import StatefulGreeting from "./components/StatefulGreeting";
-import HooksCounter from "./components/HooksCounter";
+import EventBinding from "./components/EventBinding";
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting name="John" />
-      {/* <StatefulGreeting name="Anna" /> */}
-      {/* <HooksCounter name="Mike" /> */}
+      <EventBinding />
+    
     </div>
   );
 }
--- react-components/src/components/StatefulGreeting.js
+++ react-components/src/components/StatefulGreeting.js
@@ -1,28 +1,34 @@
 import React from "react";
 
-export default class StatefulGreeting extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
+class StatefulGreeting extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+    
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!",
+        });
+        console.log(this.state.introduction);
+    }
 
-  setCount = () => {
-    this.setState(prevState => {
-      return { count: prevState.count + 1 }
-    })
-  }
-
-  render() {
-    return (
-      <div className="greeting">
-        <h1>
-          Hello, {this.props.name}, I'm a stateful component!
-        </h1>
-        <h2>You've clicked {this.state.count} times</h2>
-        <button onClick={this.setCount}>Increment Count</button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.state.introduction} {this.props.greeting}
+                </h1>
+                <button onClick={() => this.handleClick()}>
+                    {this.state.buttonText}
+                </button>
+            </div>
+        )
+    }
 }
+
+export default StatefulGreeting;
